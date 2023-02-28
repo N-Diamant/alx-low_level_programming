8066c21874d2e4e9acb15ff86ce5dab04788e533
N-Diamant <98282909+N-Diamant@users.noreply.github.com>
2023-02-28 07:47:35 +0200

    task0 updated

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,8 +1,9 @@
 #include "main.h"
 
 /**
-*set the number in the pointer to 98
-*Return: 0
+*reset_to_98 - updates the value of a pointer to , setting it to 98
+*@n:Pointer to the value to be updated
+*Return: void
 */
 void reset_to_98(int *n)
 {
