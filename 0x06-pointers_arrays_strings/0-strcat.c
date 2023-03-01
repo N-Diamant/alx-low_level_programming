7fb199ca23f41fadf332f5f1a1583207bda4171c
N-Diamant <98282909+N-Diamant@users.noreply.github.com>
2023-03-01 09:02:07 +0200

    task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+*_strcat - a funtion that concatinates strings
+*@dest:the destination string
+*@src:the source string
+*Return: Nothing
+*/
+
+void char *_strcat(char *dest, char *src){
+    int i;
+
+    int j = strlen(s);
+    for (i=0;src[i] != '\0';i++){
+        dest[i+j] = src[i];
+    }
+    dest[i+j] = '\0';
+    return;
+}
