f3c0d06ccba24e19cb4de6d26715f5bfb2460406
N-Diamant <98282909+N-Diamant@users.noreply.github.com>
2023-03-02 17:48:07 +0200

    task4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,18 @@
+/**
+ * reverse_array - reverse the contents of an array of integers
+ * @a: array of integers to be reversed
+ * @n: number of elements in the array
+ *
+ * Return: void
+ */
+void reverse_array(int *a, int n)
+{
+    int i, temp;
+
+    for (i = 0; i < n / 2; i++)
+    {
+        temp = a[i];
+        a[i] = a[n - i - 1];
+        a[n - i - 1] = temp;
+    }
+}
